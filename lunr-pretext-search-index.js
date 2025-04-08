da0a080e932583bb828906ba56232dda1832378e
Aaron Greicius <greicius@gmail.com>
2025-04-08 15:09:38 -0500

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1159,7 +1159,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.5",
   "title": "Subspace sums",
-  "body": " Subspace sums   Vector operations on sets   Let be an -vector space. Given subsets of , we define their sum , denoted or , as . Similarly, given a set and scalar , we define the scalar multiple  as .     Sum of subspaces   Let be subspaces of the -vector space , and let be their sum.    is a subspace of .     is the smallest subspace of satisfying for all in the following sense: if is a subspace of satisfying for all , then .          We follow .  Since for all , and since , we see that .  Assume . By definition, we have . But then for any scalars , we have , where for all . Since each is a subspace, we have for all . We conclude that , as desired.    For any , we have , where . Since for all , we see that . Since was an arbitrary element of , we conclude that for all .  Now assume is a subspace of that contains for all . We will show that . Indeed, given any , we have for some vectors , where . Since for all , we have for all ; since is closed under addition, we have , as desired.        Sums of subspaces   Let and consider the following subsets of : .   Prove that each is a subspace of .    Identify as a familiar subspace of . Justify your answer.    Identify as a familiar subspace of . Justify your answer.    Identify as a familiar subspace of . Justify your answer.       Blah d     Direct sum of subspaces   Let be subspaces of the -vector space . We say is the direct sum of the subspaces , denoted , or , if the following conditions hold.    .    For each there is a unique choice of vectors such that . In other words if we have , where for all , then for all .   We call a direct sum decomposition of .     Direct sum   Let and consider the following subspaces of : .   Show that and .    Show that .            Transpose matrix   Given a matrix , its transpose is the matrix whose -th entry is -th entry of for all .     Direct sum of matrix spaces   Let , the space of all matrices with entries in , and consider the following subsets of : . In other words, is the set of all symmetric matrices of and is the set of all skew-symmetric matrices of .   Prove that and are subspaces of .    Prove that .            Direct sum of subspaces   Let be subspaces of the -vector space , and let . The following statements are equivalent.        If with for all , then for all . Using logical shorthand: .           "
+  "body": " Subspace sums   Vector operations on sets   Let be an -vector space. Given subsets of , we define their sum , denoted or , as . Similarly, given a set and scalar , we define the scalar multiple  as .     Sum of subspaces   Let be subspaces of the -vector space , and let be their sum.    is a subspace of .     is the smallest subspace of satisfying for all in the following sense: if is a subspace of satisfying for all , then .          We follow .  Since for all , and since , we see that .  Assume . By definition, we have . But then for any scalars , we have , where for all . Since each is a subspace, we have for all . We conclude that , as desired.    For any , we have , where . Since for all , we see that . Since was an arbitrary element of , we conclude that for all .  Now assume is a subspace of that contains for all . We will show that . Indeed, given any , we have for some vectors , where . Since for all , we have for all ; since is closed under addition, we have , as desired.        Sums of subspaces   Let and consider the following subsets of : .   Prove that each is a subspace of .    Identify as a familiar subspace of . Justify your answer.    Identify as a familiar subspace of . Justify your answer.    Identify as a familiar subspace of . Justify your answer.       Blah d     Direct sum of subspaces   Let be subspaces of the -vector space . We say is the direct sum of the subspaces , denoted , or , if the following conditions hold.    .    For each there is a unique choice of vectors such that . In other words if we have , where for all , then for all .   We call a direct sum decomposition of .     Direct sum   Let and consider the following subspaces of : .   Show that and .    Show that .            Direct sum decomposition of   Consider the space of -tuples with coefficients in . For each , let be the set of all -tuples of the form . In other words is the set of all -tuples whose -th coordinates are equal to zero for all .  Prove that . You may take for granted that is a subspace of for all .    Given we have , where . It follows that . Furthermore, the decomposition is unique: if with for all , then since is the only vector with a nonzero -th coordinate, we must have . Since the decompotision is unique, we conclude that .     Direct sum of subspaces   Let be subspaces of the -vector space , and let . The following statements are equivalent.        If with for all , then for all . Using logical shorthand: .             Let and be subspaces of the -vector space , and let . The following statements are equivalent.    .     .       We prove both implications separately.   Implication: (1) (2)  Assume . Since for , we have . It remains to prove the reverse inclusion . Given , we have and , and thus is a decomposition of in terms of and . Since , we conclude from that . Thus .    Implication: (2) (1)  Now assume . Using , it suffices to show that if with and , then . If with , then we have , and hence that and . Since , we conclude that , as desired.      Matrix notation    Matrix whose -th entry is     -th entry of the matrix    Let be a field. We adopt the following notation for matrices of .   Matrix-building notation  The notation denotes the matrix of whose -th entry is for all and . When there is no danger of confusion, this notation will be shortened to .    Matrix entry notation  Given a matrix , for all and , denotes the -th entry of .        Transpose matrix   Given a matrix , its transpose is the matrix whose -th entry is the -th entry of for all . In other words, is the unique matrix of satisfying for all and .     Direct sum of matrix spaces   Let , the space of all matrices with entries in , and consider the following subsets of : . In other words, is the set of all symmetric matrices of and is the set of all skew-symmetric matrices of .   Prove that and are subspaces of .    Prove that .           "
 },
 {
   "id": "d_vec_ops_sets",
@@ -1207,31 +1207,58 @@ var ptx_lunr_docs = [
   "body": " Direct sum   Let and consider the following subspaces of : .   Show that and .    Show that .          "
 },
 {
-  "id": "d_transpose",
+  "id": "eg_direct_sum_Fn",
   "level": "2",
-  "url": "s_subspace_sum.html#d_transpose",
-  "type": "Definition",
-  "number": "1.5.6",
-  "title": "Transpose matrix.",
-  "body": " Transpose matrix   Given a matrix , its transpose is the matrix whose -th entry is -th entry of for all .   "
-},
-{
-  "id": "eg_direct_sum_matrices",
-  "level": "2",
-  "url": "s_subspace_sum.html#eg_direct_sum_matrices",
+  "url": "s_subspace_sum.html#eg_direct_sum_Fn",
   "type": "Example",
-  "number": "1.5.7",
-  "title": "Direct sum of matrix spaces.",
-  "body": " Direct sum of matrix spaces   Let , the space of all matrices with entries in , and consider the following subsets of : . In other words, is the set of all symmetric matrices of and is the set of all skew-symmetric matrices of .   Prove that and are subspaces of .    Prove that .          "
+  "number": "1.5.6",
+  "title": "Direct sum decomposition of <span class=\"process-math\">\\(F^n\\)<\/span>.",
+  "body": " Direct sum decomposition of   Consider the space of -tuples with coefficients in . For each , let be the set of all -tuples of the form . In other words is the set of all -tuples whose -th coordinates are equal to zero for all .  Prove that . You may take for granted that is a subspace of for all .    Given we have , where . It follows that . Furthermore, the decomposition is unique: if with for all , then since is the only vector with a nonzero -th coordinate, we must have . Since the decompotision is unique, we conclude that .   "
 },
 {
   "id": "th_subspace_direct_sum",
   "level": "2",
   "url": "s_subspace_sum.html#th_subspace_direct_sum",
   "type": "Theorem",
-  "number": "1.5.8",
+  "number": "1.5.7",
   "title": "Direct sum of subspaces.",
   "body": " Direct sum of subspaces   Let be subspaces of the -vector space , and let . The following statements are equivalent.        If with for all , then for all . Using logical shorthand: .          "
+},
+{
+  "id": "cor_subspace_dir_sum",
+  "level": "2",
+  "url": "s_subspace_sum.html#cor_subspace_dir_sum",
+  "type": "Corollary",
+  "number": "1.5.8",
+  "title": "",
+  "body": "  Let and be subspaces of the -vector space , and let . The following statements are equivalent.    .     .       We prove both implications separately.   Implication: (1) (2)  Assume . Since for , we have . It remains to prove the reverse inclusion . Given , we have and , and thus is a decomposition of in terms of and . Since , we conclude from that . Thus .    Implication: (2) (1)  Now assume . Using , it suffices to show that if with and , then . If with , then we have , and hence that and . Since , we conclude that , as desired.    "
+},
+{
+  "id": "d_matrix_notation",
+  "level": "2",
+  "url": "s_subspace_sum.html#d_matrix_notation",
+  "type": "Definition",
+  "number": "1.5.9",
+  "title": "Matrix notation.",
+  "body": " Matrix notation    Matrix whose -th entry is     -th entry of the matrix    Let be a field. We adopt the following notation for matrices of .   Matrix-building notation  The notation denotes the matrix of whose -th entry is for all and . When there is no danger of confusion, this notation will be shortened to .    Matrix entry notation  Given a matrix , for all and , denotes the -th entry of .      "
+},
+{
+  "id": "d_transpose",
+  "level": "2",
+  "url": "s_subspace_sum.html#d_transpose",
+  "type": "Definition",
+  "number": "1.5.10",
+  "title": "Transpose matrix.",
+  "body": " Transpose matrix   Given a matrix , its transpose is the matrix whose -th entry is the -th entry of for all . In other words, is the unique matrix of satisfying for all and .   "
+},
+{
+  "id": "eg_direct_sum_matrices",
+  "level": "2",
+  "url": "s_subspace_sum.html#eg_direct_sum_matrices",
+  "type": "Example",
+  "number": "1.5.11",
+  "title": "Direct sum of matrix spaces.",
+  "body": " Direct sum of matrix spaces   Let , the space of all matrices with entries in , and consider the following subsets of : . In other words, is the set of all symmetric matrices of and is the set of all skew-symmetric matrices of .   Prove that and are subspaces of .    Prove that .          "
 },
 {
   "id": "appendix-notation",
